2efc49c936d008a7ed25544f907fc96493992b0e
John Brunton <john.brunton@converge.io>
2020-10-20 22:43:10 +0100

    add issues table

---
+++ app/src/views/Issues.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="issues">
+    <h1>Issues</h1>
+
+    <table>
+      <tr v-for="issue in issues" v-bind:key="issue.key">
+        <td>{{ issue.key }}</td>
+        <td>{{ issue.title }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Issues",
+  data: () => ({
+    issues: [
+      { key: 'DEMO-101', title: 'Demo Issue 101' },
+      { key: 'DEMO-102', title: 'Demo Issue 102' }
+    ]
+  })
+});
+</script>
