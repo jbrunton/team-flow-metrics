5ac21f7ad1d950188e213165351ab4e8ca76c163
John Brunton <john.brunton@converge.io>
2020-10-24 18:40:14 +0100

    share connection

--- api/app.ts
+++ api/app.ts
@@ -5,19 +5,28 @@ const bodyParser = require('body-parser')
 
 import "reflect-metadata";
 
+const { createConnection } = require('typeorm')
+
 const app = express()
 app.use(bodyParser.json());
 
-console.log('Configuring routers...');
-glob.sync('./routers/*_router.js').forEach(function(file) {
-  const { routerPath, router, environments } = require(path.resolve(file));
-  const applyRouter = !environments || environments.includes(process.env.NODE_ENV);
-  if (applyRouter) {
-    console.log(`  Registering router ${file} at ${routerPath}`);
-    app.use(routerPath, router);
-  } else {
-    console.log(`  Skipping router ${file} for environment ${process.env.NODE_ENV}`);
-  }
-});
+async function configure() {
+  console.log('Configuring routers...');
+  glob.sync('./routers/*_router.js').forEach(function(file) {
+    const { routerPath, router, environments } = require(path.resolve(file));
+    const applyRouter = !environments || environments.includes(process.env.NODE_ENV);
+    if (applyRouter) {
+      console.log(`  Registering router ${file} at ${routerPath}`);
+      app.use(routerPath, router);
+    } else {
+      console.log(`  Skipping router ${file} for environment ${process.env.NODE_ENV}`);
+    }
+  });
+
+  console.log('Creating connection...');
+  await createConnection();
+}
 
 module.exports = app
+
+configure();
--- api/routers/issues_router.js
+++ api/routers/issues_router.js
@@ -1,23 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {createConnection} = require('typeorm')
+const {getRepository} = require('typeorm')
 const {Issue} = require('../models/entities/Issue')
 
 router.get('/', async (req, res) => {
-  const connection = await createConnection()
-  const issueRepository = connection.getRepository(Issue)
-  const issues = await issueRepository.find()
+  const issues = await getRepository(Issue).find()
   res.json({
     issues: issues
   })
-  await connection.close()
-
-  // res.json({
-  //   issues: [
-  //     { key: 'DEMO-101', title: 'Demo Issue 101' },
-  //     { key: 'DEMO-102', title: 'Demo Issue 102' }
-  //   ]
-  // })
 })
 
 module.exports = {
