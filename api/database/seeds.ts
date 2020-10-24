1e1a90d8d4c8a3748eebcf7b316d341160316659
John Brunton <john.brunton@converge.io>
2020-10-24 17:54:04 +0100

    add seeds file

---
+++ api/database/seeds.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {Issue} from "../models/entities/Issue";
+
+createConnection().then(async connection => {
+    const issue = new Issue();
+    issue.key = "DEMO-101";
+    issue.title = "Demo Issue 101";
+    await connection.manager.save(issue);
+}).catch(error => console.log(error));
