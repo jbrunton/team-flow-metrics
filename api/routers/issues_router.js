c161b0fc230176e4037ed5cf18a6a0407e9d49de
John Brunton <john.brunton@converge.io>
2020-10-24 18:29:34 +0100

    return issues from database

--- api/routers/issues_router.js
+++ api/routers/issues_router.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const {createConnection} = require('typeorm')
+const {Issue} = require('../models/entities/Issue')
 
 router.get('/', async (req, res) => {
+  const connection = await createConnection()
+  const issueRepository = connection.getRepository(Issue)
+  const issues = await issueRepository.find()
   res.json({
-    issues: [
-      { key: 'DEMO-101', title: 'Demo Issue 101' },
-      { key: 'DEMO-102', title: 'Demo Issue 102' }
-    ]
+    issues: issues
   })
+  await connection.close()
+
+  // res.json({
+  //   issues: [
+  //     { key: 'DEMO-101', title: 'Demo Issue 101' },
+  //     { key: 'DEMO-102', title: 'Demo Issue 102' }
+  //   ]
+  // })
 })
 
 module.exports = {
