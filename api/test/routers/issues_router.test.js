28a26613821483f6d3322dcb6101ab2106f29114
John Brunton <john.brunton@converge.io>
2020-10-24 12:54:49 +0100

    add test for issues router

--- api/test/routers/issues_router.test.js
+++ api/test/routers/issues_router.test.js
@@ -1,5 +1,17 @@
+const request = require('supertest')
+const app = require('../../app')
+
 describe('issues_router', () => {
-  it('test', () => {
-    expect(true).toBeTruthy();
+  it('should return a list of issues', async () => {
+    const res = await request(app)
+      .get('/issues')
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual({
+      issues: [
+        { key: 'DEMO-101', title: 'Demo Issue 101' },
+        { key: 'DEMO-102', title: 'Demo Issue 102' }
+      ]
+    })
   })
 })
+
