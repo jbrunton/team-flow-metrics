c7aaaa1c7c6af2571ce4dcf34ce9dc33678f60c9
John Brunton <john.brunton@converge.io>
2020-11-01 10:07:08 +0000

    format

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="app">
-    
     <div class="container">
       <b-navbar>
         <template slot="brand">
           <b-navbar-item tag="router-link" :to="{ path: '/' }">
-            Team Flow Metrics          
+            Team Flow Metrics
           </b-navbar-item>
         </template>
         <template slot="start">
@@ -22,10 +21,8 @@
       </b-navbar>
     </div>
 
-
     <div class="container">
       <router-view />
     </div>
-
   </div>
 </template>
--- client/src/main.ts
+++ client/src/main.ts
@@ -2,13 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
-
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
 Vue.config.productionTip = false;
 
-Vue.use(Buefy)
+Vue.use(Buefy);
 
 new Vue({
   router,
