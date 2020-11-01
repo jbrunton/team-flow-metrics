990d52f92a36ae2222a58e6636606bfc0dca5e61
John Brunton <john.brunton@converge.io>
2020-11-01 09:44:04 +0000

    add buefy

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,33 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/issues">Issues</router-link>
+    
+    <div class="container">
+      <b-navbar>
+        <template slot="brand">
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+            Team Flow Metrics          
+          </b-navbar-item>
+        </template>
+        <template slot="start">
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+            Home
+          </b-navbar-item>
+          <b-navbar-item tag="router-link" :to="{ path: '/about' }">
+            About
+          </b-navbar-item>
+          <b-navbar-item tag="router-link" :to="{ path: '/issues' }">
+            Issues
+          </b-navbar-item>
+        </template>
+      </b-navbar>
     </div>
-    <router-view />
+
+
+    <div class="container">
+      <router-view />
+    </div>
+
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
