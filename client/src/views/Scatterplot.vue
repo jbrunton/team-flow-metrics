d971b77c7b9ec3d54a8467672b43d5d4fc65d175
John Brunton <john.brunton@converge.io>
2020-11-01 17:06:41 +0000

    display cycle times

--- client/src/views/Scatterplot.vue
+++ client/src/views/Scatterplot.vue
@@ -10,6 +10,7 @@
 /* global google */
 
 import Vue from "vue";
+import axios from "axios";
 
 export default Vue.extend({
   name: "Issues",
@@ -19,18 +20,20 @@ export default Vue.extend({
     };
   },
   mounted() {
-    // axios.get(`/api/issues`).then(response => {
-    //   this.issues = response.data.issues.map(issue => {
-    //     return {
-    //       key: issue.key,
-    //       title: issue.title,
-    //       started: this.formatDate(issue.started),
-    //       completed: this.formatDate(issue.completed),
-    //       cycleTime: this.formatNumber(issue.cycleTime)
-    //     };
-    //   });
-    // });
-    this.loadCharts();
+    axios.get(`/api/issues`).then(response => {
+      this.issues = response.data.issues
+        .filter(issue => issue.completed)
+        .map(issue => {
+          return {
+            key: issue.key,
+            title: issue.title,
+            started: new Date(issue.started),
+            completed: new Date(issue.completed),
+            cycleTime: issue.cycleTime
+          };
+        });
+      this.loadCharts();
+    });
   },
 
   methods: {
@@ -39,20 +42,30 @@ export default Vue.extend({
       google.charts.setOnLoadCallback(this.drawChart);
     },
     drawChart() {
-      const data = google.visualization.arrayToDataTable([
-        ["Age", "Weight"],
-        [8, 12],
-        [4, 5.5],
-        [11, 14],
-        [4, 5],
-        [3, 3.5],
-        [6.5, 7]
+      const issueData = this.issues.map(issue => [
+        issue.completed,
+        issue.cycleTime
       ]);
+      console.log(issueData);
+      console.log(issueData.map(row => row[0].getTime()));
+      const data = new google.visualization.DataTable();
+      data.addColumn("date", "Completed");
+      data.addColumn("number", "Cycle Time");
+      data.addRows(issueData);
+      // const data = google.visualization.arrayToDataTable([
+      //   ["Age", "Weight"],
+      //   [8, 12],
+      //   [4, 5.5],
+      //   [11, 14],
+      //   [4, 5],
+      //   [3, 3.5],
+      //   [6.5, 7]
+      // ]);
 
       const options = {
         title: "Age vs. Weight comparison",
-        hAxis: { title: "Age", minValue: 0, maxValue: 15 },
-        vAxis: { title: "Weight", minValue: 0, maxValue: 15 },
+        //hAxis: { title: "Age", minValue: 0, maxValue: 15 },
+        //vAxis: { title: "Weight", minValue: 0, maxValue: 15 },
         legend: "none"
       };
 
