dde21b35860fdda99ea4c8ec3ac3095b05ed1d65
John Brunton <john.brunton@converge.io>
2020-11-01 10:45:27 +0000

    use buefy table

--- client/src/views/Issues.vue
+++ client/src/views/Issues.vue
@@ -2,34 +2,8 @@
   <div class="issues">
     <h1>Issues</h1>
 
-    <table>
-      <thead>
-        <tr>
-          <th>
-            Key
-          </th>
-          <th>
-            Title
-          </th>
-          <th>
-            Started
-          </th>
-          <th>
-            Completed
-          </th>
-          <th>
-            Cycle Time
-          </th>
-        </tr>
-      </thead>
-      <tr v-for="issue in issues" v-bind:key="issue.key">
-        <td>{{ issue.key }}</td>
-        <td>{{ issue.title }}</td>
-        <td>{{ formatDate(issue.started) }}</td>
-        <td>{{ formatDate(issue.completed) }}</td>
-        <td>{{ formatNumber(issue.cycleTime) }}</td>
-      </tr>
-    </table>
+    <b-table :data="issues" :columns="columns"></b-table>
+
   </div>
 </template>
 
@@ -42,12 +16,27 @@ export default Vue.extend({
   name: "Issues",
   data() {
     return {
-      issues: []
+      issues: [],
+      columns: [
+        { field: 'key', label: 'Key' },
+        { field: 'title', label: 'Title' },
+        { field: 'started', label: 'Started' },
+        { field: 'completed', label: 'Completed' },
+        { field: 'cycleTime', label: 'Cycle Time' }
+      ]
     };
   },
   mounted() {
     axios.get(`/api/issues`).then(response => {
-      this.issues = response.data.issues;
+      this.issues = response.data.issues.map(issue => {
+        return {
+          key: issue.key,
+          title: issue.title,
+          started: this.formatDate(issue.started),
+          completed: this.formatDate(issue.completed),
+          cycleTime: this.formatNumber(issue.cycleTime)
+        };
+      });
     });
   },
   methods: {
