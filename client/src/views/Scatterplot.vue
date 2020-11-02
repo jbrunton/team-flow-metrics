6f5e8718b0edff2f80392cfb2f78920c34b899dc
John Brunton <john.brunton@converge.io>
2020-11-02 19:32:50 +0000

    add datepicker to scatterplot

--- client/src/views/Scatterplot.vue
+++ client/src/views/Scatterplot.vue
@@ -2,6 +2,18 @@
   <div class="issues">
     <h1>Scatterplot</h1>
 
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <b-field label="Date Range">
+            <b-datepicker
+                placeholder="Click to select..."
+                v-model="dates"
+                range>
+            </b-datepicker>
+        </b-field>
+      </div>
+    </div>
+
     <div id="chart_div" style="width: 900px; height: 500px;"></div>
   </div>
 </template>
@@ -17,22 +29,37 @@ export default Vue.extend({
   data() {
     return {
       chartOps: {},
-      chartData: []
+      chartData: [],
+      dates: []
     };
   },
   mounted() {
-    axios.get("/api/charts/scatterplot").then(response => {
-      console.log(response.data);
-      this.chartData = response.data.chartData;
-      this.chartOpts = response.data.chartOpts;
-      this.loadCharts();
-    });
+    this.loadCharts();
   },
 
   methods: {
+    initChart() {
+      const now = new Date();
+      const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      const toDate = new Date(today);
+      toDate.setDate(toDate.getDate() + 1);
+      const fromDate = new Date(toDate);
+      fromDate.setDate(toDate.getDate() - 90);
+      this.dates = [fromDate, toDate];
+    },
+    fetchData() {
+      const fromDate = this.dates[0];
+      const toDate = this.dates[1];
+
+      axios.get(`/api/charts/scatterplot?fromDate=${fromDate.toString()}&toDate=${toDate.toString()}`).then(response => {
+        this.chartData = response.data.chartData;
+        this.chartOpts = response.data.chartOpts;
+        this.drawChart();
+      });
+    },
     loadCharts() {
       google.charts.load("current", { packages: ["corechart"] });
-      google.charts.setOnLoadCallback(this.drawChart);
+      google.charts.setOnLoadCallback(this.initChart);
     },
     drawChart() {
       const data = new google.visualization.DataTable(this.chartData);
@@ -40,6 +67,12 @@ export default Vue.extend({
         document.getElementById("chart_div")
       );
       chart.draw(data, this.chartOpts);
+    }
+  },
+
+  watch: {
+    dates (dates) {
+      this.fetchData();
     }
   }
 });
