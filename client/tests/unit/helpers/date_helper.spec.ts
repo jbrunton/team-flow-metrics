10f1d3fab33a5d7f48d5aa1367a1a0b3e203a2f3
John Brunton <john.brunton@converge.io>
2020-11-03 20:17:54 +0000

    format

--- client/tests/unit/helpers/date_helper.spec.ts
+++ client/tests/unit/helpers/date_helper.spec.ts
@@ -1,4 +1,7 @@
-import { formatDateRange, getDefaultDateRange } from "../../../src/helpers/date_helper";
+import {
+  formatDateRange,
+  getDefaultDateRange
+} from "../../../src/helpers/date_helper";
 
 describe("getDefaultDateRange", () => {
   it("returns dates spanning the last 30 days", () => {
@@ -12,6 +15,8 @@ describe("formatDateRange", () => {
   it("formats the given range", () => {
     const fromDate = new Date(2020, 0, 1);
     const toDate = new Date(2020, 1, 10);
-    expect(formatDateRange("en-GB", [fromDate, toDate])).toEqual("Jan 1, 2020 - Feb 10, 2020");
+    expect(formatDateRange("en-GB", [fromDate, toDate])).toEqual(
+      "Jan 1, 2020 - Feb 10, 2020"
+    );
   });
 });
